63ba4e1fe31440fa05125c104d9a987eed8b6723
bravexample <laiyongdian@gmail.com>
2023-04-28 13:20:55 -0400

    basic things

---
+++ main.c
@@ -0,0 +1,24 @@
+// Main process for the final project
+
+// Libraries
+#include "init.h"
+
+// Main function
+int main()
+{
+    // Initialize
+    project_init();
+
+    // Main loop
+    // while (1)
+    // {
+    //     // Update
+    //     // project_update();
+    // }
+    
+    // Print the map for debugging
+    print_map();
+
+    // Exit program
+    return 0;
+}
