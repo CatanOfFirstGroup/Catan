ba4027e067869fe6371cc3f53b7749c52edc2eea
bravexample <laiyongdian@gmail.com>
2023-06-12 15:16:30 -0400

    add NPC

--- game/game.h
+++ game/game.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
+#include <unistd.h> // for NPC to slow down
 
 
 void game_init(WINDOW *board, WINDOW *player, WINDOW *progress, GameState *state);
